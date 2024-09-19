13463f5e12a17335760b0acc39bd0d7519516f27
Heero <techheero@gmail.com>
2024-09-19 00:09:00 -0500

    adding leaves to the tree

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // we are building a balanced binary tree with Naruto characters
-// put all teams in right places but read in alphabetical order 
-
+// put all teams in right places but read in alphabetical order
 
 //  create class for data
 //  class Node{
@@ -12,65 +11,105 @@
 // };
 
 class TreeNode {
-    // this will establish the leader or parent with no children
-    constructor(name) {
-        this.name = name;
-        this.left = null;
-        this.right = null;
-    }
+  // this will establish the leader or parent with no children
+  constructor(name) {
+    this.name = name;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 class BalanceTree {
-    constructor(){
-        this.root = null;
-        this.nodes = []; //this will hold all the nodes and sort at the end
-    }
-    // there are pros and cons to balanced no vs later read and write speed
-    addNode(name){
-        this.nodes.push(name); //hokage, leader or ninja to the arrary
-    }
-    // the method to build a balanced tree from arrary
-    buildBalanceTree(){
-        // sort in alphabetical order to find best bts structure or pathway
-        this.nodes.sort();
-        // recursive method are counting down to a point without being promted
-        this.root = this._buildBalancedTreeRecursive(this.nodes, 0, this.nodes.length - 1);
+  constructor() {
+    this.root = null;
+    this.nodes = []; //this will hold all the nodes and sort at the end
+  }
+  // there are pros and cons to balanced no vs later read and write speed
+  addNode(name) {
+    this.nodes.push(name); //hokage, leader or ninja to the arrary
+  }
+  // the method to build a balanced tree from arrary
+  buildBalanceTree() {
+    // sort in alphabetical order to find best bts structure or pathway
+    this.nodes.sort();
+    // recursive method are counting down to a point without being promted
+    this.root = this._buildBalancedTreeRecursive(
+      this.nodes,
+      0,
+      this.nodes.length - 1
+    );
+  }
 
+  // next function
+  // sortedNodes will be used to arrange the array in alphabetical order
+  //start will be the index of the current subarray always starts at 0
+  //end the ending index of the current subarray intial minus 1
+  _buildBalancedTreeRecursive(sortedNodes, start, end) {
+    // the rule of cool is start is greater than end return null
+    if (start > end) {
+      return null;
     }
 
-    // next function 
-    _buildBalancedTreeRecursive(???){
+    //  we need to get the middle element to make the root for the next node
+    // we need two variable one for middle and to place a node in the middle
+    const mid = Math.floor((start + end) / 2);
+    const node = new TreeNode(sortedNodes[mid]);
 
-        returns null; //because it will build tree other wise
+    // so now check the tree recursively from left to right
+    // left we minus from the Array
+    node.left = this._buildBalancedTreeRecursive(sortedNodes, start, mid - 1);
+    // i we gop right we add to the mid
+    node.right = this._buildBalancedTreeRecursive(sortedNodes, mid + 1, end);
+    // this should be the constructed left end or mid tree or null
+    return node;
+  }
+//   setting up the path to display the tree
+inorderPathway(node){
+    if(node !==null){
+        this.inorderPathway(node.left);
+        this.log(node.name);
+        this.inorderPathway(node.right);
     }
+    console.log('waffle house')
+}
 }
 
+// msake teams with path way
+// add hokage and leaders
+const teams = new BalanceTree();
+teams.addNode('Hokage');
+teams.addNode('Kakashi');
+teams.addNode('Kurenai');
+teams.addNode('Asuma');
+teams.addNode('Guy');
 
+// how this works display tree in alphabeitcal order 
+console.log('put my shit straight')
+teams.inorderPathway(teams.root)
 
 //*******************************************
 
 // this is how the root is established and pointed to
-class TheVillage {
-    constructor(){
-        this.root = null
-    }
-    // method to insert Hokage as the root
-    insertHokage(hokage){
-        // this should be inserted into all empty trees? 
-        if(this.root === null){
-            this.root = new TreeNode(hokage)
-        }
-    }
+// class TheVillage {
+//   constructor() {
+//     this.root = null;
+//   }
+//   // method to insert Hokage as the root
+//   insertHokage(hokage) {
+//     // this should be inserted into all empty trees?
+//     if (this.root === null) {
+//       this.root = new TreeNode(hokage);
+//     }
+//   }
 
-    //now to put the hokage in the village 
-    insertLeader(hokage, leader){
-        // attach leader to hokage (parent)
-        const hokagNode = this.findLeader(this.root, hokage)
-        // now that hokge which is the parent is know we can
-        // attach children aka leader 
-        if(hokagNode){
-            hokagNode.left = this.insertTeamLead(hokagNode.left, leader);
-        }
-    }
-}
-
+//   //now to put the hokage in the village
+//   insertLeader(hokage, leader) {
+//     // attach leader to hokage (parent)
+//     const hokagNode = this.findLeader(this.root, hokage);
+//     // now that hokge which is the parent is know we can
+//     // attach children aka leader
+//     if (hokagNode) {
+//       hokagNode.left = this.insertTeamLead(hokagNode.left, leader);
+//     }
+//   }
+// }
